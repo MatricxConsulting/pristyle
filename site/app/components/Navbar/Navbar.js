9b3bd8108df08334381cc04c9b7c3c01c1db5cf8
brianbiendou <biendou-djay@insa-toulouse.fr>
2026-05-18 10:44:31 +0200

    Refactor code structure for improved readability and maintainability

--- site/app/components/Navbar/Navbar.js
+++ site/app/components/Navbar/Navbar.js
@@ -16,9 +16,9 @@ const NAV_ITEMS = [
 ];
 
 const TRANSPARENT_LOGO = {
-  src: "/images/logos/nouveaulogo.png",
-  width: 1439,
-  height: 823,
+  src: "/images/logos/logotextenoir.png",
+  width: 1622,
+  height: 970,
 };
 
 const SCROLLED_LOGO = {
